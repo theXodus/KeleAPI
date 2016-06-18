2e78743a1cf006cd274d774d03b617de7e03424b
theXodus <jprice1990@gmail.com>
2016-06-18 16:57:00 -0500

    Add messaging to Kele

--- lib/kele.rb
+++ lib/kele.rb
@@ -24,4 +24,22 @@ class Kele
     response = self.class.get("#{@base_uri}/mentors/#{id}/student_availability", headers: { "authorization" => @auth_token })
     tp response.parsed_response # tp calls table_print
   end
+
+  def get_messages(page = nil)
+    response = self.class.get("#{@base_uri}/message_threads", headers: { "authorization" => @auth_token })
+    body = response.parsed_response
+    if page
+      self.class.get("#{@base_uri}/message_threads", { headers: { "authorization" => @auth_token }, body: { page: page }})
+    else
+      all_threads = (1..(response["count"]/10 + 1)).map do |p|
+        self.class.get("#{@base_uri}/message_threads", { headers: { "authorization" => @auth_token }, body: { page: p }})
+      end
+    end
+  end
+
+  def create_message(subject, text)
+    message = { headers: { "authorization" => @auth_token}, body: { user_id: get_me['id'], recipient_id: get_me["current_enrollment"]["mentor_id"], subject: subject, "stripped-text" => text} }
+    response = self.class.post("#{@base_uri}/messages", message)
+    response.parsed_response
+  end
 end
